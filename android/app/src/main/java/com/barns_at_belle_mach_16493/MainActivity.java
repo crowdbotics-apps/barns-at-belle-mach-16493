7736026484522a18bdc6fd72e2c0881a2b8bb355
Crowdbotics <team@crowdbotics.com>
2020-05-01 14:13:15 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/barns_at_belle_mach_16493/MainActivity.java
@@ -0,0 +1,15 @@
+package com.barns_at_belle_mach_16493;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "barns_at_belle_mach_16493";
+    }
+}
